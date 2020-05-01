bd2c44dd7d7b3d9ad25d31e7cb4899d0f5c4cd89
Christian <newc0495@student.faytechcc.edu>
2020-05-01 13:52:51 -0400

    add all Test files

---
+++ Employees/ClassLibrary/Employee.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class Employee
+    {
+        // Fields
+        private string _name;
+        private string _phone;
+        private int _age;
+
+        // Properties
+        public string Name { get => _name; set => _name = value; }
+        public string Phone { get => _phone; set => _phone = value; }
+        public int Age { get => _age; set => _age = value; }
+
+        // Constructor
+        public Employee()
+        {
+            Name = "";
+            Phone = "";
+            Age = 0;
+        }
+
+        public Employee(string name, string phone, int age)
+        {
+            Name = name;
+            Phone = phone;
+            Age = age;
+        }
+
+        // Methods
+        public static void BuildEmployee(List<Employee> employeeList)
+        {
+            Employee thisEmployee = new Employee();
+            bool error = false;
+            do
+            {
+                Console.WriteLine("Employee's first name: ");
+                string firstName = Util.UserPrompt();
+                Console.WriteLine("Employee's last name: ");
+                string lastName = Util.UserPrompt();
+                thisEmployee.Name = $"{lastName}, {firstName}";
+                Console.WriteLine($"You've entered {thisEmployee.Name}");
+
+                Console.WriteLine("Employee's Phone #: ");
+                thisEmployee.Phone = Util.UserPrompt();
+                Console.WriteLine($"You've entered {thisEmployee.Phone}");
+
+                Console.WriteLine("Employee's Age: ");
+                thisEmployee.Age = Util.MakeInt(Util.UserPrompt());
+                if (thisEmployee.Age < 18)
+                {
+                    error = true;
+                    Console.WriteLine("Something went wrong. Please start over.");
+                }
+                else
+                {
+                    error = false;
+                }
+            } while (error == true);
+
+            employeeList.Add(thisEmployee);
+            Console.WriteLine($"The Employee's reference ID is {employeeList.IndexOf(thisEmployee)}");
+        }
+
+        public static string ShowEmployee(Employee selectedEmployee)
+        {
+            return $"Employee Name: {selectedEmployee.Name}" +
+                $"\nEmployee Phone #: {selectedEmployee.Phone}" +
+                $"\nEmployee Age: {selectedEmployee.Age}";
+        }
+    }
+}
---
+++ Employees/ClassLibrary/Util.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class Util
+    {
+        public static string DisplayMenu()
+        {
+            return "1. Enter Employee's Info" +
+                "\n2. Display Employee Info" +
+                "\n3. Display Average Age" +
+                "\n4. Exit Program";
+        }
+
+        public static string UserPrompt()
+        {
+            Console.Write("Enter: ");
+            string input = Console.ReadLine();
+            return input;
+        }
+
+        public static int MakeInt(string input)
+        {
+            int output = 0;
+            if (int.TryParse(input, out output))
+            {
+                return output;
+            }
+            else
+            {
+                output = -1;
+                return output;
+            }
+        }
+
+        public static string InvalidInput()
+        {
+            return "Invalid input. Please try again.";
+        }
+
+        public static double GetAvgAge(List<Employee> employeeList)
+        {
+            int count = 0;
+            int ageTotal = 0;
+            double average = 0.0;
+            if (employeeList.Count > 0)
+            {
+                foreach (Employee employee in employeeList)
+                {
+                    count++;
+                    ageTotal += employee.Age;
+                }
+                average = ageTotal / count;
+                return average;
+            }
+            else
+            {
+                average = 0;
+                Console.WriteLine("There are no employees to average!");
+                return average;
+            }
+        }
+    }
+}
