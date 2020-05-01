a7960538e25f6c62a5a13625b95c2fed0f7fb6ad
Christian <newc0495@student.faytechcc.edu>
2020-05-01 13:42:49 -0400

    add Module 4 Part 3 files

---
+++ AttendanceMarch16/ClassLibrary/Mob.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Mob
+    {
+        // Fields
+        private string _name;
+        private int _atk;
+        private int _def;
+
+        // Properties
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int Atk
+        {
+            get
+            {
+                return _atk;
+            }
+            set
+            {
+                _atk = value;
+            }
+        }
+
+        public int Def
+        {
+            get
+            {
+                return _def;
+            }
+            set
+            {
+                _def = value;
+            }
+        }
+
+        // Auto Property
+        public int Loot { get; set; }
+        
+        // Constructor
+        public Mob()
+        {
+            Name = "";
+            Atk = 0;
+            Def = 0;
+            Loot = 0;
+        }
+
+        public Mob(string name, int atk, int def, int loot)
+        {
+            Name = name;
+            Atk = atk;
+            Def = def;
+            Loot = loot;
+        }
+
+    }
+}
---
+++ AttendanceMarch16/ClassLibrary/Weapon.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Weapon
+    {
+
+        // Fields
+        private string _name;
+        private int _dmg;
+
+        // Properties
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int Dmg
+        {
+            get
+            {
+                return _dmg;
+            }
+            set
+            {
+                _dmg = value;
+            }
+        }
+
+        // Auto Property
+        public int Rarity { get; set; }
+
+        // Constructor
+        public Weapon()
+        {
+            Name = "";
+            Dmg = 0;
+            Rarity = 0;
+        }
+
+        public Weapon(string name, int dmg, int rarity)
+        {
+            Name = name;
+            Dmg = dmg;
+            Rarity = rarity;
+        }
+
+    }
+}
