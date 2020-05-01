a277e00df09425613013d798a841e5eed0b0f3e7
Christian <newc0495@student.faytechcc.edu>
2020-05-01 13:35:11 -0400

    add Module 3 files

---
+++ FallingDistance/ConsoleUI/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+/* 2/25/20
+ * CSC 153
+ * Christian New
+ * App allows the user to enter the amount of
+ * time a object has fallen and displays the
+ * distance the object fell.
+ */
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        const double GRAV = 9.8;
+
+        static void Main(string[] args)
+        {
+            var exit = false;
+
+            do
+            {
+                Console.WriteLine("--- Main Menu ---");
+                Console.WriteLine("1. Enter");
+                Console.WriteLine("2. Exit");
+                Console.WriteLine();
+                Console.WriteLine("Please enter an option: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        double timeToCalculate = FallingTime();
+                        Calculate(timeToCalculate);
+                        break;
+                    case 2:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine($"'{choice}' is not a valid option. Please try again.");
+                        Console.WriteLine();
+                        break;
+                }
+
+            } while (exit == false);
+        }
+
+        static double FallingTime()
+        {
+            Console.WriteLine("Please enter the the amount of time the object has fallen: ");
+            double time = double.Parse(Console.ReadLine());
+            Console.WriteLine($"You said the object has been falling {time} second(s).");
+            return time;
+        }
+
+        static void Calculate(double time)
+        {
+            double distanceFallen = 0.5 * (GRAV * (time * time));
+            Console.WriteLine($"The object has fallen {distanceFallen} meters.");
+        }
+    }
+}
---
+++ RetailPriceCalculator/ConsoleUI/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 2/25/20
+ * CSC 153
+ * Christian New
+ * App lets the user enter an item's cost and
+ * its markup to display the item's price.
+ */
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exit = false;
+
+            do
+            {
+                Console.WriteLine("--- Main Menu ---");
+                Console.WriteLine("1. Enter");
+                Console.WriteLine("2. Exit");
+                Console.WriteLine();
+                Console.WriteLine("Enter an option: ");
+                string choiceString = Console.ReadLine();
+                int choice;
+                if (int.TryParse(choiceString, out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Calculator();
+                            break;
+                        case 2:
+                            exit = true;
+                            break;
+                        default:
+                            Console.WriteLine();
+                            Console.WriteLine($"'{choice}' is not a valid option. Please try again.");
+                            Console.WriteLine();
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"'{choiceString}' is not a valid option. Please try again.");
+                    Console.WriteLine();
+                }
+            } while (exit == false);
+        }
+
+        static void Calculator()
+        {
+            Console.WriteLine("Please enter the wholesale cost of the item: ");
+            string costString = Console.ReadLine();
+            decimal cost;
+            if (decimal.TryParse(costString, out cost))
+            {
+                Console.WriteLine();
+                Console.WriteLine($"You entered ${cost} as the cost.");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Please enter the percentage markup of the item: ");
+            string markupString = Console.ReadLine();
+            decimal markup;
+            if (decimal.TryParse(markupString, out markup))
+            {
+                Console.WriteLine();
+                Console.WriteLine($"You entered {markup}% for the markup.");
+                Console.WriteLine();
+            }
+
+            decimal price = cost + (cost * (markup / 100));
+
+            Console.WriteLine("Calculating...");
+            Console.WriteLine();
+            Console.WriteLine($"The price for this item is: ${price:N2}");
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
